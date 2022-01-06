68c8b866114c621da6e0f638d9877907ac3ae208
yuraaaaaa <80416736+yuraaaaaa@users.noreply.github.com>
2022-01-06 16:09:48 +0200

    Update copy.js

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,3 +2,8 @@ export const copy = () => {
     return app.gulp.src(app.path.src.files)
         .pipe(app.gulp.dest(app.path.build.files))
 }
+
+export const copyCss = () => {
+    return app.gulp.src(app.path.src.css)
+        .pipe(app.gulp.dest(app.path.build.css))
+}
